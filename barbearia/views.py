1e93d0317561517ee753ca03cae06be7b8c87fdd
Pedro Henrique dos Santos Mateus <pedrobox10@live.com>
2023-11-29 12:23:22 -0300

    notificações e botão de deslogar

--- barbearia/views.py
+++ barbearia/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
 def principal(request):
     return render(request,'principal.html')
 
 def opcoes(request):
-    return render(request, 'opcoes.html')
+    template = loader.get_template('opcoes.html')
+    if request.user.is_authenticated:
+        context = {
+                'user': request.user
+        }
+        return HttpResponse(template.render(context, request))
+    
+    return HttpResponse(template.render())
